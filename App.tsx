d7e4ab1731fcb18dd554a4287a9eee68b543d3f0
Ivad <yvesyvad@gmail.com>
2023-07-29 22:46:51 +0200

    feat: add custom component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, View } from "react-native";
+import { View } from "react-native";
 import Button from "./components/Button";
 
 const App = () => {
   return (
     <View className="flex-1 items-center justify-center">
       <StatusBar style="auto" />
-      <Button onPress={() => Alert.alert("Hello words")}>Hello world</Button>
+      <View className="flex gap-9">
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+      </View>
     </View>
   );
 };
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,15 @@
 import { FC, PropsWithChildren } from "react";
-import { Pressable, Text } from "react-native";
+import { Alert, Pressable, Text } from "react-native";
 
 interface ButtonProps {
   onPress?: () => void;
 }
 const Button: FC<PropsWithChildren<ButtonProps>> = ({ children, onPress }) => {
   return (
-    <Pressable className="px-5 py-3 bg-orange-500 rounded-md" onPress={onPress}>
+    <Pressable
+      className="px-10 py-3 bg-orange-400 rounded-md mt-5"
+      onPress={() => Alert.alert(`${children} pressed`)}
+    >
       <Text className="text-white">{children}</Text>
     </Pressable>
   );
