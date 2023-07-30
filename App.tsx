6d4e0dacf2c0efc5cf86797cd3c4b48848207d0a
Ivad <yvesyvad@gmail.com>
2023-07-30 11:12:17 +0200

    feat: addd state and props

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
 
 const App = () => {
+  const [count, setCount] = useState<number>(0);
   return (
     <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-white text-lg">Hello react native</Text>
       <StatusBar style="auto" />
+      <Text className="text-white text-lg">Button has been clicked {count} times</Text>
+      <Button title="Click me" onPress={() => setCount((prevCount) => prevCount + 1)} />
     </View>
   );
 };
